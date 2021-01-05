a8bb58d34d094d8b4f765dcafbc93ef3d7f84c07
John Schmidt <mail@johnschmidt.de>
2021-01-05 12:12:59 +0100

    minor changes

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,5 @@
 import Error from 'components/Error'
-import Layout from 'components/Layout'
 
 export default function Error404() {
-  return (
-    <Layout>
-      <Error code='404' />
-    </Layout>
-  )
+  return <Error code='404' />
 }
