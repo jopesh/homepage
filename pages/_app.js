10d965325b5d473f0c7ed7820c77e00bfe8ec824
John Schmidt <mail@johnschmidt.de>
2021-01-06 11:28:46 +0100

    updated favicons

--- pages/_app.js
+++ pages/_app.js
@@ -24,22 +24,26 @@ function HomepageApp({ Component, pageProps }) {
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/apple-touch-icon.png'
+          href='/apple-touch-icon.png?v=oLnz5kRpkO'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/favicon-32x32.png'
+          href='/favicon-32x32.png?v=oLnz5kRpkO'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/favicon-16x16.png'
+          href='/favicon-16x16.png?v=oLnz5kRpkO'
         />
-        <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
+        <link rel='manifest' href='/site.webmanifest?v=oLnz5kRpkO' />
+        <link
+          rel='mask-icon'
+          href='/safari-pinned-tab.svg?v=oLnz5kRpkO'
+          color='#000000'
+        />
         <meta name='msapplication-TileColor' content='#8c22d0' />
         <meta name='theme-color' content='#ffffff' />
       </Head>
