b32753f92ade63063864112cd57eefed6c91246a
John Schmidt <mail@johnschmidt.de>
2021-01-11 23:25:59 +0100

    json support

--- components/BlockCode.js
+++ components/BlockCode.js
@@ -1,5 +1,6 @@
 import Prism from 'prismjs'
 import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-json'
 
 const BlockCode = ({ code = '', language = 'javascript' }) => {
   const html = Prism.highlight(code, Prism.languages[language], language)
