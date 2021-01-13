6547487778a2874597dc69e1e253420022673618
John Schmidt <mail@johnschmidt.de>
2021-01-13 11:27:36 +0100

    footer improvements

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,16 @@
 import { Copyright } from 'phosphor-react'
-import { convert } from 'number-words'
-import { SiDevDotTo, SiGithub, SiTwitter } from 'react-icons/si'
+import { FaDev, FaGithub, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
-  const currentYear = convert(new Date().getFullYear())
   return (
     <footer className='max-w-screen-lg px-6 mx-auto my-24'>
-      <div className='flex items-center mb-4 space-x-3 sm:justify-center'>
+      <div className='flex items-center mb-4 space-x-1.5 sm:justify-center -ml-2 sm:ml-0'>
         <a
           className='p-2'
           href='https://twitter.com/jope_sh'
           target='_blank'
           rel='noopener noreferrer'>
-          <SiTwitter size='1.5em' aria-label='Twitter' />
+          <FaTwitter size='1.5em' aria-label='Twitter' />
           <span className='sr-only'>Twitter profile</span>
         </a>
         <a
@@ -20,7 +18,7 @@ const Footer = () => {
           href='https://github.com/jopesh'
           target='_blank'
           rel='noopener noreferrer'>
-          <SiGithub size='1.5em' aria-label='GitHub' />
+          <FaGithub size='1.5em' aria-label='GitHub' />
           <span className='sr-only'>Github profile</span>
         </a>
         <a
@@ -28,16 +26,15 @@ const Footer = () => {
           href='https://dev.to/jopesch'
           target='_blank'
           rel='noopener noreferrer'>
-          <SiDevDotTo size='1.5em' aria-label='Dev.to' />
+          <FaDev size='1.5em' aria-label='Dev.to' />
           <span className='sr-only'>Dev.to profile</span>
         </a>
       </div>
       <div className='text-sm text-gray-500 dark:text-gray-400 sm:text-center'>
         <p className='inline-flex items-center space-x-1'>
-          <Copyright />
-          <span>John Schmidt in {currentYear}</span>
+          <span>Made by John Schmidt in {new Date().getFullYear()}</span>
         </p>
-        <p>Made in Hamburg</p>
+        <p>in Hamburg</p>
       </div>
     </footer>
   )
