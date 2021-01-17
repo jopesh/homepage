ee1a205b169a2c3a9dd09481279d2957aacf543a
John Schmidt <mail@johnschmidt.de>
2021-01-17 17:24:52 +0100

    different format

--- utils/formatDate.js
+++ utils/formatDate.js
@@ -1,11 +1,5 @@
 import { format } from 'date-fns'
 
-export default function formatDate(input) {
-  let date
-  if (input) {
-    date = new Date(input)
-  } else {
-    date = new Date()
-  }
-  return format(date, 'EEEE, do LLLL yyyy')
+export default function formatDate(input = new Date()) {
+  return format(new Date(input), 'PPP')
 }
