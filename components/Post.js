0701f72b5c70828c134e42eed8052c4125f606f1
John Schmidt <mail@johnschmidt.de>
2021-01-27 23:18:20 +0100

    add reading-time to post

--- components/Post.js
+++ components/Post.js
@@ -1,19 +1,38 @@
+import { useRef } from 'react'
+import Image from 'next/image'
+import useReadingTime from 'use-reading-time'
 import formatDate from 'utils/formatDate'
 import BlockContent from './BlockContent'
 
 const Post = ({ data }) => {
+  const text = useRef()
+  const { readingTime } = useReadingTime(text)
   const { title, publishedAt, body, summary, slug, mainImage, category } = data
   const date = formatDate(publishedAt)
   return (
-    <article className='mx-auto my-6 prose lg:my-8 md:prose-lg dark:prose-dark'>
+    <article
+      className='mx-auto my-6 prose lg:my-8 md:prose-lg dark:prose-dark'
+      ref={text}>
       <h1>{title}</h1>
-      {category === 'blog' && (
-        <time
-          className='inline-block mt-3 text-gray-500 md:mt-6 dark:text-gray-400'
-          dateTime={publishedAt}>
-          {date}
-        </time>
-      )}
+      <div className='flex items-center my-8 space-x-3 text-sm'>
+        <div className='flex flex-grow-0 flex-shrink-0 overflow-hidden rounded-full'>
+          <Image
+            src='/images/portrait.jpg'
+            height='36'
+            width='36'
+            layout='fixed'
+            alt='Portrait of John'
+          />
+        </div>
+        <div className='flex flex-col flex-grow sm:flex-row sm:justify-between'>
+          <span className='font-medium'>by John Schmidt</span>
+          <div>
+            <span>{readingTime} min read</span>
+            <span className='mx-2'>–</span>
+            <time dateTime={publishedAt}>{date}</time>
+          </div>
+        </div>
+      </div>
       {body && <BlockContent blocks={body} className='mt-6' />}
     </article>
   )
