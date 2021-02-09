2b0ab53b6d6c9ac2b73656f3ddbb9dde928e04c8
John Schmidt <mail@johnschmidt.de>
2021-02-09 15:32:31 +0100

    revert webpack 5 beta

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   images: {
     domains: ['cdn.sanity.io'],
-    deviceSizes: [439, 632, 756, 828, 1080, 1512],
+    deviceSizes: [439, 632, 756, 828, 1080, 1512]
   },
   webpack: (config, { dev, isServer }) => {
     if (isServer) {
@@ -16,19 +13,19 @@ module.exports = {
       Object.assign(config.resolve.alias, {
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
+        'react-dom': 'preact/compat'
       })
     }
 
     return config
   },
-  async redirects() {
+  async redirects () {
     return [
       {
         source: '/project/:slug',
         destination: '/work/:slug',
-        permanent: true,
-      },
+        permanent: true
+      }
     ]
-  },
+  }
 }
