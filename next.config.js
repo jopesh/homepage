04e8edb65ed8f4f088194466e2cb9bf08b4a43eb
John Schmidt <mail@johnschmidt.de>
2021-02-12 13:39:33 +0100

    back to regular react

--- next.config.js
+++ next.config.js
@@ -3,20 +3,10 @@ module.exports = {
     domains: ['cdn.sanity.io'],
     deviceSizes: [439, 632, 756, 828, 1080, 1512]
   },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config, { isServer }) => {
     if (isServer) {
       require('./utils/generateSitemap')
     }
-
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
-
     return config
   },
   async redirects () {
