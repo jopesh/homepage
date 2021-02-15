21d308fd548d05ff4ac732e52178d28472b58cf8
John Schmidt <mail@johnschmidt.de>
2021-02-15 20:31:24 +0100

    margins

--- components/Layout.js
+++ components/Layout.js
@@ -6,11 +6,15 @@ const Layout = ({ children }) => {
     <div>
       <a
         href='#main'
-        className='absolute p-3 bg-white opacity-0 focus-visible:z-50 focus-visible:opacity-100 top-6 left-4 dark:bg-black'>
+        className='absolute p-3 bg-white opacity-0 focus-visible:z-50 focus-visible:opacity-100 top-3 left-4 dark:bg-black'
+      >
         Skip to main content
       </a>
       <Header />
-      <main id='main' className='max-w-screen-lg px-6 mx-auto'>
+      <main
+        id='main'
+        className='max-w-screen-lg px-6 mx-auto mt-6 md:mt-8 lg:mt-10'
+      >
         {children}
       </main>
       <Footer />
