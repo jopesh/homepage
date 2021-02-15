1200e90e1298acb9c4223700015bf5a37a081376
John Schmidt <mail@johnschmidt.de>
2021-02-15 20:31:20 +0100

    text adjustments

--- components/Intro.js
+++ components/Intro.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import { ArrowDown, ChatCenteredText } from 'phosphor-react'
 
 const Intro = () => {
-  const handleClick = (e) => {
+  const handleClick = e => {
     e.preventDefault()
     plausible('Lead')
     window.location.href = 'mailto:mail@johnschmidt.de'
@@ -22,8 +22,7 @@ const Intro = () => {
         </div>
         <h1 className='text-3xl font-black tracking-tight sm:text-5xl md:text-6xl'>
           Hi. I'm John.
-          <br />
-          Front-end developer.
+          <br />A front-end developer.
         </h1>
         <p className='max-w-xl sm:text-lg lg:max-w-2xl md:text-xl lg:text-2xl'>
           I aim to create fast, simple and accessible web experiences for
@@ -33,13 +32,14 @@ const Intro = () => {
           <a
             onClick={handleClick}
             href='mailto:mail@johnschmidt.de'
-            className='inline-flex items-center px-6 py-4 space-x-3 text-xl font-semibold text-white bg-indigo-700 dark:text-white hover:bg-indigo-600 hover:text-white ring-indigo-700 dark:ring-indigo-300 ring-offset-2 hover:no-underline'>
+            className='inline-flex items-center px-6 py-4 space-x-3 text-xl font-semibold text-white bg-indigo-700 dark:text-white hover:bg-indigo-600 hover:text-white ring-indigo-700 dark:ring-indigo-300 ring-offset-2 hover:no-underline'
+          >
             <span>Get in touch</span>
             <ChatCenteredText focusable='false' weight='bold' size='24' />
           </a>
         </div>
         <p className='flex flex-col items-center tracking-tight text-center text-gray-500 dark:text-gray-400'>
-          <span className='mb-4'>Explore my thoughts &amp; work</span>
+          <span className='mb-4 text-lg'>Explore my thoughts &amp; work</span>
           <ArrowDown size='24' />
         </p>
         <p className='sr-only'>Scroll down</p>
