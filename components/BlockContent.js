5a476c412bfcf8a3ec0f77482d39dc2be7980846
John Schmidt <mail@johnschmidt.de>
2021-02-15 20:31:03 +0100

    improved anchor links

--- components/BlockContent.js
+++ components/BlockContent.js
@@ -12,53 +12,55 @@ const BlockContent = ({ ...args }) => (
     {...args}
     serializers={{
       types: {
-        block: (props) => {
+        block: props => {
           const regex = /h\d$/ms
           const match = regex.test(props.node.style)
           const slug = slugify(props.children.toString(), {
-            lower: true,
+            lower: true
           })
           if (match) {
             return createElement(props.node.style, {
               id: slug,
               style: {
-                scrollMarginTop: '6rem',
+                scrollMarginTop: '6rem'
               },
               children: [
-                props.children,
-                <div key='spacer' className='inline-block w-0.5 h-0' />,
-                <a
-                  className='inline-flex items-center justify-center p-1.5'
-                  href={`#${slug}`}
-                  key='anchor'>
-                  <span className='sr-only'>
-                    Anchor link for: {props.children.toString()}
-                  </span>
-                  <Hash className='w-3 h-3 md:w-4 md:h-4' weight='bold' />
-                </a>,
-              ],
+                <div className='flex items-start'>
+                  <a
+                    className='px-1 py-2.5 -ml-5 xl:py-3.5 md:px-2 md:-ml-7'
+                    href={`#${slug}`}
+                    key='anchor'
+                  >
+                    <span className='sr-only'>
+                      Anchor link for: {props.children.toString()}
+                    </span>
+                    <Hash className='w-3 h-3' weight='bold' />
+                  </a>
+                  {props.children}
+                </div>
+              ]
             })
           } else if (props.node.style === 'blockquote') {
             return <blockquote>{props.children}</blockquote>
           } else return <p>{props.children}</p>
         },
-        code: (props) => (
+        code: props => (
           <BlockCode
             code={props.node.code}
             language={props.node.language}
             filename={props.node.filename}
           />
         ),
-        image: (props) => <BlockImage image={props} width={812} />,
-        meta: (props) => <BlockStack node={props.node} />,
+        image: props => <BlockImage image={props} width={812} />,
+        meta: props => <BlockStack node={props.node} />
       },
       marks: {
-        link: (props) => (
+        link: props => (
           <a href={props.mark.href} target='_blank' rel='noopener noreferrer'>
             {props.children}
           </a>
-        ),
-      },
+        )
+      }
     }}
   />
 )
