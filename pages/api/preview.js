95b944e18e2bd2abe7522b15a12d19b3198d7a8b
John Schmidt <mail@johnschmidt.de>
2021-03-03 22:51:47 +0100

    authenticate with token

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,31 +1,19 @@
+import groq from "groq"
+import { getClient } from "lib/sanity"
+
 export default async (req, res) => {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
-  if (
-    req.query.secret !== process.env.SANITY_PREVIEW_SECRET ||
-    !req.query.slug
-  ) {
+  if (req.query.secret !== process.env.SANITY_PREVIEW_SECRET) {
     return res.status(401).send("Unauthorized")
-  } else {
-    res.setPreviewData({})
-    res.redirect(`/${req.query.category}/${req.query.slug}`)
   }
-  // Fetch the headless CMS to check if the provided `slug` exists
-  // getPostBySlug would implement the required fetching logic to the headless CMS
-  // const post = await getClient().fetch(
-  //   groq`*[_type == "post" && slug.current == $slug][0]`,
-  //   {
-  //     slug: req.query.slug,
-  //   }
-  // )
-
-  // If the slug doesn't exist prevent preview mode from being enabled
-  // if (!post) {
-  //   return res.status(401).json({ message: 'Invalid slug' })
-  // }
-
-  // Enable Preview Mode by setting the cookies
-
-  // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
+  const post = await getClient(true).fetch(
+    groq`*[_type == "post" && slug.current == $slug][0]{slug, category->{slug}}`,
+    {
+      slug: req.query.slug,
+    }
+  )
+  if (!post) {
+    return res.status(401).json({ message: "Invalid slug" })
+  }
+  res.setPreviewData({})
+  res.redirect(`/${post.category.slug.current}/${post.slug.current}`)
 }
