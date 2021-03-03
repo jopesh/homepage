28942c5b24aa71e4f220e600557673d87cae058b
John Schmidt <mail@johnschmidt.de>
2021-03-03 11:59:13 +0100

    post layout changes

--- components/Post.js
+++ components/Post.js
@@ -11,28 +11,28 @@ const Post = ({ data }) => {
   const date = formatDate(publishedAt)
   return (
     <article
-      className="mx-auto my-6 prose xl:prose-lg lg:my-8 dark:prose-dark"
+      className="mx-auto my-6 prose lg:prose-lg lg:my-8 dark:prose-dark"
       ref={text}
     >
       <h1>{title}</h1>
-      <div className="flex items-center my-6 space-x-3 text-sm">
+      <div className="flex items-center my-6 space-x-3">
         <div className="flex flex-grow-0 flex-shrink-0 overflow-hidden rounded-full">
           <Image
             src="/images/portrait.jpg"
-            height="32"
-            width="32"
+            height="48"
+            width="48"
             layout="fixed"
             priority
             alt="Portrait of John"
           />
         </div>
-        <div className="flex flex-col flex-grow font-medium sm:flex-row sm:justify-between">
+        <div className="flex flex-col leading-snug flex-grow sm:flex-row sm:justify-between text-sm sm:text-base">
           <span>John Schmidt</span>
-          <div>
+          <span>
             <ReadingTime text={text} />
             <span className="mx-2">–</span>
             <time dateTime={publishedAt}>{date}</time>
-          </div>
+          </span>
         </div>
       </div>
       {body && <BlockContent blocks={body} className="mt-6" />}
