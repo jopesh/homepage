bc45e3febc653955e76b379fd93b11a48a203293
John Schmidt <mail@johnschmidt.de>
2021-03-04 23:04:32 +0100

    preact

--- next.config.js
+++ next.config.js
@@ -3,9 +3,19 @@ module.exports = {
     domains: ["cdn.sanity.io"],
     deviceSizes: [439, 632, 756, 828, 1080, 1512],
   },
-  webpack: (config, { isServer }) => {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { dev, isServer }) => {
     if (isServer) {
       require("./utils/generateSitemap")
+    }
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      })
     }
     return config
   },
