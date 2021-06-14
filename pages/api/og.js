cdf1a73c9f9c6051d9375eaa54b64c4374983fea
John Schmidt <mail@johnschmidt.de>
2021-06-14 21:00:40 +0200

    feat: og images served by mql

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,42 +1,30 @@
-import puppeteer from "puppeteer-core"
-import chromium from "chrome-aws-lambda"
+import mql from "@microlink/mql"
 
-const isDev = process.env.NODE_ENV === "development"
+async function takeScreenshot(url) {
+  const { data } = await mql(url, {
+    meta: false,
+    screenshot: true,
+    type: "jpeg",
+    viewport: {
+      width: 1200,
+      height: 628,
+      deviceScaleFactor: 1,
+      isMobile: false,
+    },
+  })
+  return data.screenshot
+}
 
 export default async (req, res) => {
   const { query } = req
-  let browser = null
-  try {
-    browser = await chromium.puppeteer.launch({
-      executablePath: isDev
-        ? puppeteer.executablePath()
-        : await chromium.executablePath,
-      headless: isDev ? true : chromium.headless,
-      args: isDev ? [] : chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      ignoreHTTPSErrors: true,
-    })
-
-    const page = await browser.newPage()
-    await page.setViewport({
-      width: 1200,
-      height: 628,
-    })
-    await page.goto(
-      query.t
-        ? `http://${req.headers.host}/og?title=${encodeURIComponent(query.t)}`
-        : `http://${req.headers.host}/og`
-    )
-    const screenshot = await page.screenshot({ encoding: "binary" })
-    res.setHeader("content-type", "image/png")
-    res.setHeader("cache-control", "public, max-age=31536000, immutable")
-    res.status(200).send(screenshot)
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error })
-  } finally {
-    if (browser) {
-      await browser.close()
-    }
+  if (!query.t) {
+    const screenshot = await takeScreenshot(`https://johnschmidt.de/og`)
+    // return res.status(200).json(screenshot)
+    return res.redirect(screenshot.url)
   }
+  const screenshot = await takeScreenshot(
+    `https://johnschmidt.de/og?title=${query.t}`
+  )
+  // return res.status(200).json(screenshot)
+  return res.redirect(screenshot.url)
 }
