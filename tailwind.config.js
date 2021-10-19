f310822faca5ad1c3843bdd5a68eb045a36e86e5
John Schmidt <mail@johnschmidt.de>
2021-10-19 21:40:11 +0200

    media based dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require("tailwindcss/colors")
 
 module.exports = {
   purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
+  darkMode: "media",
   mode: "jit",
   theme: {
     extend: {
