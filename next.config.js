e16968524055af48c734a7a81019cf82c7c3d3a6
John Schmidt <mail@johnschmidt.de>
2022-02-12 18:26:35 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
-module.exports = {
-  images: {
-    domains: ["cdn.sanity.io", "iad.microlink.io"],
-    deviceSizes: [325, 439, 632, 756, 828, 1080, 1512],
-    imageSizes: [200],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require("./utils/generateSitemap")
-    }
-    return config
-  },
-  async redirects() {
-    return [
-      {
-        source: "/project/:slug",
-        destination: "/work/:slug",
-        permanent: true,
-      },
-    ]
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
--- utils/formatDate.js
+++
@@ -1,5 +0,0 @@
-import { format } from "date-fns"
-
-export default function formatDate(input = new Date()) {
-  return format(new Date(input), "PPP")
-}
