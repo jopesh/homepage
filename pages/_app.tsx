18832877c59e4f87dedd9df61d1767f42428c24d
John Schmidt <mail@johnschmidt.de>
2022-02-13 21:21:36 +0100

    scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,16 @@ import "styles/globals.css"
 
 import type { AppProps } from "next/app"
 import { DefaultSeo } from "next-seo"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        id="plausible"
+        data-domain="johnschmidt.de"
+        src="https://stats.johnschmidt.cloud/js/plausible.js"
+      />
       <DefaultSeo
         title="John Schmidt - Freelancer, educator, paramedic"
         description="Freelance web developer, educator, and paramedic. Thriving to make the web a more accessible, enjoyable place."
