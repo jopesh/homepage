b23e0e20fca7399fd70e7817b871be2d4f427b66
John Schmidt <mail@johnschmidt.de>
2022-02-14 17:00:57 +0100

    add post seo

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,11 +4,11 @@ import type { Post, Tag } from "lib/sanity.models"
 import Container from "components/container"
 import DisplayViews from "components/display-views"
 import Layout from "components/layout"
-import Link from "next/link"
 import { NextSeo } from "next-seo"
 import PortableText from "components/portable-text"
 import TagList from "components/tag-list"
 import { sanityClient } from "lib/sanity.server"
+import { urlFor } from "lib/sanity.client"
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const data: Post[] = await sanityClient.fetch(
@@ -50,6 +50,11 @@ type Props = {
 }
 
 const PostPage: NextPage<Props> = ({ data }) => {
+  const seoImage = data.seo?.image
+    ? urlFor(data.seo?.image).url()
+    : data.image
+    ? urlFor(data.image).width(1200).height(628).url()
+    : "/img/default-seo.jpg"
   return (
     <Layout>
       <NextSeo
@@ -57,9 +62,18 @@ const PostPage: NextPage<Props> = ({ data }) => {
         title={data.title}
         description={data.description}
         canonical={`https://johnschmidt.de/post/${data.slug?.current}`}
+        openGraph={{
+          images: [
+            {
+              url: seoImage,
+              width: 1200,
+              height: 628,
+            },
+          ],
+        }}
       />
       <Container>
-        <article className="prose prose-sm prose-zinc prose-a:text-indigo-700 prose-pre:-mx-4 prose-pre:rounded-none prose-pre:border prose-pre:border-zinc-100 prose-pre:bg-zinc-50 prose-pre:text-sm prose-pre:text-zinc-900 dark:prose-invert dark:prose-a:text-indigo-300 dark:prose-pre:border-zinc-800 dark:prose-pre:bg-zinc-800/30 dark:prose-pre:text-zinc-100 sm:prose-base sm:prose-pre:mx-0 sm:prose-pre:rounded md:prose-pre:-mx-8">
+        <article className="prose prose-sm prose-zinc prose-a:text-indigo-700 prose-pre:-mx-4 prose-pre:rounded-none prose-pre:border prose-pre:border-zinc-100 prose-pre:bg-zinc-50 prose-pre:text-sm prose-pre:text-zinc-900 dark:prose-invert dark:prose-a:text-indigo-300 dark:prose-pre:border-zinc-800 dark:prose-pre:bg-zinc-800/30 dark:prose-pre:text-zinc-100 sm:prose-base sm:prose-pre:mx-0 sm:prose-pre:rounded md:prose-pre:-mx-8 md:prose-pre:text-base">
           <div className="not-prose mb-2 text-xs md:text-sm">
             <DisplayViews slug={data.slug?.current || ""} />
           </div>
