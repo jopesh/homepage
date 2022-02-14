2813678e4cd8d5fdb4c493659543fcdcc11be688
John Schmidt <mail@johnschmidt.de>
2022-02-14 17:24:53 +0100

    add redirects

--- next.config.js
+++ next.config.js
@@ -9,6 +9,20 @@ const nextConfig = {
     deviceSizes: [320, 430, 640, 1280],
     imageSizes: [96],
   },
+  async redirects() {
+    return [
+      {
+        source: "/blog/:path",
+        destination: "/post/:path",
+        permanent: true,
+      },
+      {
+        source: "/work/:path",
+        destination: "/post/:path",
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
