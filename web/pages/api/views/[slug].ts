834db2836bfa544d50e4247856060bd680bd8fab
John Schmidt <mail@johnschmidt.de>
2022-02-15 15:12:55 +0100

    cleanup

--- web/pages/api/views/[slug].ts
+++ web/pages/api/views/[slug].ts
@@ -1,23 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 
+import { fetcher } from "lib/fetcher"
+
+const now = new Date()
+const [date] = now.toISOString().split("T")
+
 async function getPlausibleViews(slug: string) {
-  const [date] = new Date().toISOString().split("T")
   const url = `https://stats.johnschmidt.cloud/api/v1/stats/aggregate?site_id=johnschmidt.de&period=custom&date=2020-12-29,${date}&filters=event:page==/blog/${slug}|/work/${slug}|/post/${slug}`
-  const data = await fetch(url, {
+  return fetcher(url, {
     headers: {
       Authorization: `Bearer ${process.env.PLAUSIBLE_API_KEY}`,
       Accept: "application/json",
     },
   })
-  return data.json()
 }
 
 const viewsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { query } = req
+  const { slug } = req.query
+  if (!slug) {
+    return res.status(400).send("Bad request")
+  }
   try {
-    const data = await getPlausibleViews(query.slug as string)
+    const data = await getPlausibleViews(String(slug))
     return res.status(200).json({
-      slug: query.slug,
+      requestedSlug: slug,
+      date: now.toUTCString(),
       views: data.results.visitors.value,
     })
   } catch (error) {
