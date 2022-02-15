db706cd89662a903b161fc6058bf920c34fadbc2
John Schmidt <mail@johnschmidt.de>
2022-02-15 15:13:04 +0100

    add more languages

--- web/components/block-code.tsx
+++ web/components/block-code.tsx
@@ -3,8 +3,12 @@ import { PropsWithChildren } from "react"
 import Refractor from "react-refractor"
 import json from "refractor/lang/json"
 import jsx from "refractor/lang/jsx"
+import tsx from "refractor/lang/tsx"
+import typescript from "refractor/lang/typescript"
 
 Refractor.registerLanguage(jsx)
+Refractor.registerLanguage(tsx)
+Refractor.registerLanguage(typescript)
 Refractor.registerLanguage(json)
 
 const BlockCode: React.FC<
