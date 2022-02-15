99b274aeda84397dd4306715f8ec22078bba6f45
John Schmidt <mail@johnschmidt.de>
2022-02-15 17:06:42 +0100

    improved footer

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -3,6 +3,27 @@ import { SiDevdotto, SiGithub, SiTwitter } from "react-icons/si"
 import Container from "./container"
 import { Heart } from "phosphor-react"
 
+type SocialItemProps = {
+  icon: React.FC
+  href: string
+  label: string
+}
+
+const SocialItem: React.FC<SocialItemProps> = ({ icon, href, label }) => {
+  const Icon = icon
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex rounded p-2 text-zinc-400 hover:text-zinc-900 focus:outline-none focus-visible:text-zinc-900 focus-visible:ring dark:text-zinc-500 dark:hover:text-zinc-100 dark:focus-visible:text-zinc-100"
+    >
+      <span className="sr-only">{label}</span>
+      <Icon />
+    </a>
+  )
+}
+
 const Footer: React.FC = () => {
   return (
     <footer className="mt-10 mb-10 text-center font-mono text-xs sm:text-sm lg:mt-16">
@@ -18,33 +39,21 @@ const Footer: React.FC = () => {
           <p>2022</p>
         </div>
         <div className="flex items-center justify-center space-x-2 text-xl md:text-2xl">
-          <a
+          <SocialItem
             href="https://github.com/jopesh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex p-2 text-zinc-400 hover:text-zinc-800 dark:text-zinc-500 dark:hover:text-zinc-100"
-          >
-            <span className="sr-only">GitHub Profile</span>
-            <SiGithub />
-          </a>
-          <a
+            label="GitHub profile"
+            icon={SiGithub}
+          />
+          <SocialItem
             href="https://twitter.com/jope_sh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex p-2 text-zinc-400 hover:text-zinc-800 dark:text-zinc-500 dark:hover:text-zinc-100"
-          >
-            <span className="sr-only">Twitter Profile</span>
-            <SiTwitter />
-          </a>
-          <a
+            label="Twitter profile"
+            icon={SiTwitter}
+          />
+          <SocialItem
             href="https://dev.to/jopesh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex p-2 text-zinc-400 hover:text-zinc-800 dark:text-zinc-500 dark:hover:text-zinc-100"
-          >
-            <span className="sr-only">Dev.to Profile</span>
-            <SiDevdotto />
-          </a>
+            label="Dev.to Profile"
+            icon={SiDevdotto}
+          />
         </div>
       </Container>
     </footer>
