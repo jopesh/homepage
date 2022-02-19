a956cc4036e69ae066c62b804a811c12520d7dfe
John Schmidt <mail@johnschmidt.de>
2022-02-19 14:10:28 +0100

    fix path

--- studio/sanity-codegen.config.ts
+++ studio/sanity-codegen.config.ts
@@ -2,7 +2,7 @@ import { SanityCodegenConfig } from "sanity-codegen"
 
 const config: SanityCodegenConfig = {
   schemaPath: "./schemas/schema.js",
-  outputPath: "../lib/sanity.models.ts",
+  outputPath: "../web/lib/sanity.models.ts",
 }
 
 export default config
