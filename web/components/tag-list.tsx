ea398a77b346ac39204f108203f3abc61cac1210
John Schmidt <mail@johnschmidt.de>
2022-02-27 15:23:46 +0100

    style icons

--- web/components/tag-list.tsx
+++ web/components/tag-list.tsx
@@ -17,8 +17,8 @@ const TagList: React.FC<Props> = ({ data, className }) => {
           className="mr-[0.5em] mb-[0.5em] last:mr-0"
         >
           <Link href={`/tag/${(tag as any).slug?.current}`}>
-            <a className="flex items-center rounded-md bg-zinc-50 px-2 py-1 font-medium tracking-tight text-zinc-600 hover:bg-indigo-50 hover:text-indigo-700 focus:outline-none focus-visible:bg-indigo-50 focus-visible:text-indigo-700 focus-visible:ring dark:bg-zinc-800 dark:text-zinc-300 dark:hover:bg-indigo-900/50 dark:hover:text-indigo-200 dark:focus-visible:bg-indigo-900/50 dark:focus-visible:text-indigo-200">
-              <TagIcon className="mr-2" weight="duotone" />
+            <a className="flex items-center rounded-md bg-zinc-50 px-2 py-1.5 tracking-tight text-zinc-600 hover:bg-indigo-50 hover:text-indigo-700 focus:outline-none focus-visible:bg-indigo-50 focus-visible:text-indigo-700 focus-visible:ring dark:bg-zinc-800 dark:text-zinc-300 dark:hover:bg-indigo-900/50 dark:hover:text-indigo-200 dark:focus-visible:bg-indigo-900/50 dark:focus-visible:text-indigo-200">
+              <TagIcon className="mr-2" />
               <span className="sr-only">Tag: </span>
               <span>{(tag as any).title}</span>
             </a>
