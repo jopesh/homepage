227c5b8c03029d419fcf0b5e0bb401815569ba94
John Schmidt <mail@johnschmidt.de>
2022-04-05 11:21:03 +0200

    change config

--- web/lib/sanity.config.ts
+++ web/lib/sanity.config.ts
@@ -10,6 +10,6 @@ interface ClientConfig {
 export const sanityConfig: ClientConfig = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  useCdn: process.env.NODE_ENV === "production",
+  useCdn: process.env.NODE_ENV !== "production",
   apiVersion: "2022-02-12",
 }
