a996e9dbce12ece732cf2d7d280546d521f5154c
John Schmidt <mail@johnschmidt.de>
2022-04-12 23:38:41 +0200

    chore

--- studio/sanity-codegen.config.ts
+++
@@ -1,8 +0,0 @@
-import { SanityCodegenConfig } from "sanity-codegen"
-
-const config: SanityCodegenConfig = {
-  schemaPath: "./schemas/schema.js",
-  outputPath: "../web/lib/sanity.models.ts",
-}
-
-export default config
